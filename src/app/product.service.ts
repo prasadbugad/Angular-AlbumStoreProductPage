ca68bcff00cbab30ecb5ed4eaef40b2fd50d7c2f
Prasad Bugad <prasad.bugad28@gmail.com>
2021-10-01 17:51:41 +0530

    Module 4 add interfaces to describe data

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http'
 import 'rxjs/add/operator/map'
+import { Album } from './album'
+import { Observable } from 'rxjs/Observable';
+
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json'
@@ -8,7 +11,7 @@ export class ProductService {
     private _http: Http
   ) { }
 
-  getAlbum(id: number) {
+  getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map(response => response.json())
   }
 
